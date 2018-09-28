cb6b736d132871a7ae2d42482d3b15d849239c37
Hitchens, Francis (fh562u) <fh562u@att.com>
2018-09-28 15:52:04 -0500

    Some updates

---
+++ README.md
@@ -0,0 +1,49 @@
+# vizk8s
+
+Vizualize a K8s cluster
+
+## Overview
+
+FIXME: Write a paragraph about the library/project and highlight its goals.
+
+## Setup
+
+To get an interactive development environment run:
+
+    lein figwheel
+
+and open your browser at [localhost:3449](http://localhost:3449/).
+This will auto compile and send all changes to the browser without the
+need to reload. After the compilation process is complete, you will
+get a Browser Connected REPL. An easy way to try it is:
+
+    (js/alert "Am I connected?")
+
+and you should see an alert in the browser window.
+
+To clean all compiled files:
+
+    lein clean
+
+To create a production build run:
+
+    lein do clean, cljsbuild once min
+
+And open your browser in `resources/public/index.html`. You will not
+get live reloading, nor a REPL. 
+
+The Clojure package [com.guaranteedrate/ring-proxy "3.0.0"] needs to be downloaded and patched
+https://github.com/tailrecursion/ring-proxy/pull/14/commits/97be94dc2d4a93c1810a60019a0e02c2c0f9ef97#diff-3fe6fccb7dc3e851151e2fdc7f13ecee
+
+Then do a
+$ export LEIN_SNAPSHOTS_IN_RELEASE=1.0.0
+$ lein uberjar
+$ lein install
+
+
+
+## License
+
+Copyright © 2014 FIXME
+
+Distributed under the Eclipse Public License either version 1.0 or (at your option) any later version.
---
+++ src/vizk8s/main.cljs
@@ -0,0 +1,50 @@
+(ns vizk8s.main
+  (:require-macros [cljs.core.async.macros :refer [go]])
+  (:require [reagent.core :as reagent :refer [atom]]
+            [cljs-http.client :as http]
+            [cljs.core.async :refer [<!]]
+            [vizk8s.model :as model]
+            [vizk8s.view.navbar :as navbar]
+            [vizk8s.view.nodes :as nodes]))
+
+
+(enable-console-print!)
+
+(println "This text is printed from src/vizk8s/core.cljs. Go ahead and edit it and see reloading in action.")
+
+(go (let [response (<! (http/get "https://api.github.com/users"
+                                 {:with-credentials? false
+                                  :query-params {"since" 135}}))]
+      (prn (:status response))
+      (prn (map :login (:body response)))))
+
+(go (let [response (<! (http/get "/hello"))]
+          (prn (:body response))))
+
+;(go (let [response (<! (http/get "/api/v1/namespaces/com-att-cwdigital-shop-prod/pods"))]
+;      (prn (:body response))))
+
+;(go (def pods (<! (http/get "/api/v1/namespaces/com-att-cwdigital-shop-prod/pods"))))
+
+;(println (first (:items (:body pods))))
+
+(model/refresh-nodes model/app-state)
+
+(defn vizk8s-main [app-state]
+  [:div.content
+   [:div.header
+    [navbar/navbar app-state]]
+   [:div.nodesbar
+    [nodes/node-panel app-state]]
+   [:div.main]
+   [:div.footer]])
+
+(reagent/render-component [vizk8s-main]
+                          (. js/document (getElementById "app")))
+
+
+(defn on-js-reload []
+  ;; optionally touch your app-state to force rerendering depending on
+  ;; your application
+  ;; (swap! app-state update-in [:__figwheel_counter] inc)
+)
