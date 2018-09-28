a561b378a5cb3eba8e55f442fa43b4eed0f32249
Hitchens, Francis (fh562u) <fhitchen@gmail.com>
2018-09-28 18:41:23 -0500

    added next steps to nodes.cljs

--- src/vizk8s/view/nodes.cljs
+++ src/vizk8s/view/nodes.cljs
@@ -3,6 +3,9 @@
    [vizk8s.model :as model]))
 
 
+;(:spec (nth (get-in @vizk8s.model/app-state [:nodes])3))
+; (last (:conditions (:status (second (get-in @vizk8s.model/app-state [:nodes])))))
+
 (defn node-panel [app-state]
   (into [:ul]
         (for [{{:keys [name]} :metadata}
